7017db0e3849051d17280028164992cd0636de75
David Tang <dtang859@gmail.com>
2020-06-17 21:36:45 -0700

    Adds Local Storage adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,8 @@
-import MyRESTAdapter from './my-rest';
+// Toggle these depending on if you want to see the application
+// working with a REST API or Local Storage
+// import Adapter from './my-rest';
+import Adapter from './local-storage';
 
-export default class ApplicationAdapter extends MyRESTAdapter {
+export default class ApplicationAdapter extends Adapter {
   namespace = 'api';
 }
